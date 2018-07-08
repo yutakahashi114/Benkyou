a778a3b76697d5591b8c97a52a08c97d6a01ba79
高橋　祐羽 <yu_takahashi@INV-605noMacBook-puro.local>
2018-07-09 07:57:38 +0900

    ハノイの塔

--- hanoi/hanoi.php
+++ hanoi/hanoi.php
@@ -1,17 +1,17 @@
 <?php
 
 
-$hanoi = [ [ 1, 2, 3, 4, 5,6 ], [], [] ];
+$hanoi = [ [ 1, 2, 3, 4, 5 ], [], [] ];
+// $hanoi = [ [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ], [], [] ];
 
 // ここで、ハノイの塔を解く
 // $hanoiが$hanoi_resultと同じ感じになればOK!
 
 /**
- * 
- * @param array $hanoi ハノイの塔
+ * @param array   $hanoi  ハノイの塔
  * @param integer $height 塔の高さ
- * @param integer $now 今の塔の位置（0, 1, 2）
- * @param integer $move 塔を動かす方向（1, -1）
+ * @param integer $now    塔の現在位置（0, 1, 2）
+ * @param integer $move   塔を動かす方向（1, -1）
  */
 function hanoi($hanoi, $height, $now, $move)
 {
@@ -34,71 +34,8 @@ function hanoi($hanoi, $height, $now, $move)
     }
 }
 
-$result = hanoi($hanoi, count($hanoi[0]), 0, 2);
+$result = hanoi($hanoi, count($hanoi[0]), 0, 1);
 var_dump($result[0]);
-
-
-
-
-
-
-
-// $now = 2;
-
-// var_dump(hanoi($hanoi, $now));
-
-// function hanoi($hanoi, $now) {
-//     $one = ($now + 1) % 3;
-//     $two = ($now + 2) % 3;
-
-//     if (count($hanoi[0]) === 0) {
-//         if ((count($hanoi[1]) === 0) || (count($hanoi[2]) === 0)) {
-//             return $hanoi;
-//         }
-//     }
-//     if (count($hanoi[$now]) > 1) {
-//         if ($hanoi[$now][0] === 1) {
-//             if (count($hanoi[$one]) === 0) {
-//                 array_unshift($hanoi[$two], arrayshift($hanoi[$one]));            
-//                 $move = $two;
-//             } elseif (count($hanoi[$two]) === 0) {
-//                 array_unshift($hanoi[$one], array_shift($hanoi[$two]));            
-//                 $move = $one;
-//             } elseif ($hanoi[$one][0] > $hanoi[$two][0]) {
-//                 array_unshift($hanoi[$one], array_shift($hanoi[$two]));
-//                 $move = $one;
-//             } else {
-//                 array_unshift($hanoi[$two], array_shift($hanoi[$one]));
-//                 $move = $two;
-//             }
-//         } else {
-//             if (count($hanoi[$one]) === 0) {
-//                 array_unshift($hanoi[$now], array_shift($hanoi[$two]));
-//                 $move = $now;                
-//             } elseif ($hanoi[$one][0] === 1) {
-//                 array_unshift($hanoi[$two], array_shift($hanoi[$one]));
-//                 $move = $two;
-//             } else {
-//                 array_unshift($hanoi[$now], array_shift($hanoi[$two]));
-//                 $move = $now;
-//             }
-//         }
-//     } else {
-//         if (count($hanoi[$one]) === 0) {
-//             array_unshift($hanoi[$now], array_shift($hanoi[$two]));
-//             $move = $now;                
-//         } elseif ($hanoi[$one][0] === 1) {
-//             array_unshift($hanoi[$two], array_shift($hanoi[$one]));
-//             $move = $two;
-//         } else {
-//             array_unshift($hanoi[$now], array_shift($hanoi[$two]));
-//             $move = $now;
-//         }
-//     }    
-//     return hanoi($hanoi, $move);
-// }
-
-
 
 $hanoi_result = [ [], [ 1, 2, 3, 4, 5 ], [] ];
 // var_dump($hanoi_result);
