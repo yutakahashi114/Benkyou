3105d988ee29c7637a1016795da79a5800f85320
高橋　祐羽 <yu_takahashi@inoue.inv.local>
2018-08-22 08:46:40 +0900

    Webサーバスクリプト

---
+++ shell/server/httpServer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+while read line
+do
+    echo "LINE: $line"
+    if [[ ${line} =~ (^GET .*$) ]]
+    then
+        file=$(echo ${line} | awk '{print $2;}')
+        if [ -r $file ]
+        then
+            cat $file
+        else
+            echo "404: ${file} is not found"
+        fi
+    fi
+done
