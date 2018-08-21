5c25429e64c94601305d72cb83f1d9ffea68abc8
高橋　祐羽 <yu_takahashi@inoue.inv.local>
2018-08-22 08:50:24 +0900

    微修正

--- shell/warning/warning.sh
+++ shell/warning/warning.sh
@@ -2,7 +2,7 @@
 
 capacity=$(sh oredf.sh | awk '{print $5;}' | sed -e 's/\([0-9]\{1,3\}\)%$/ \1/' | sed -n 2p)
 
-while [ ${capacity} -lt 80 ]
+while [ !${capacity} -ge 80 ]
 do
     capacity=$(sh oredf.sh | awk '{print $5;}' | sed -e 's/\([0-9]\{1,3\}\)%$/ \1/' | sed -n 2p)
 done
